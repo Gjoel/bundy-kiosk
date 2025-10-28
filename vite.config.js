3c80dfcabae4871a1f805ae3a2db215c11ed3614
Joel Gibson <joel@wslr.com.au>
2025-10-28 17:13:43 +1100

    wip: local changes before rebase

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Bundy-kiosk/",   // exact repo name & case
-  build: {
-    outDir: "dist",        // let Vite use default /assets/* output
-  },
+  base: "/bundy-kiosk/",   // repo path in all-lowercase
+  build: { outDir: "dist" }
 });
